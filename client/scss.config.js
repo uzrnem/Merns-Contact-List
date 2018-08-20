d5a69b4f358b5d68fe31867ea604a6b2c4779d03
uzrnem <bhagyeshsunilpatel@gmail.com>
2018-08-20 10:50:42 +0530

    CL-0007
    Seprate Webpack config added to compile scss

---
+++ client/scss.config.js
@@ -0,0 +1,47 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    admin: './scss/main.scss'
+  },
+  output: {
+    path: __dirname + '/../',
+    filename: "garage/dummy.min.js"
+  },
+  module: {
+    rules: [
+      /*
+      your other rules for JavaScript transpiling go in here
+      */
+      { // css / sass / scss loader for webpack
+        test: /\.(css|sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "public/compiled/css/[name].bundle.css",
+      allChunks: true,
+    }),
+  ],
+  stats: {
+    colors: true,
+    hash: false,
+    version: false,
+    timings: true,
+    assets: true,
+    chunks: false,
+    modules: false,
+    reasons: false,
+    children: false,
+    source: true,
+    errors: true,
+    errorDetails: true,
+    warnings: true,
+    publicPath: true
+  }
+};
