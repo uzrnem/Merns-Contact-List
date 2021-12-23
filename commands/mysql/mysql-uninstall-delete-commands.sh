61e91e12d44e1f0dbf2f69c7f10183d93eb0d9e4
Bhagyesh Sunil Patel <bhagyeshsunilpatel@gmail.com>
2021-12-23 09:11:17 +0530

    created seperate branch

--- commands/mysql/mysql-uninstall-delete-commands.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get remove --purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
-
-sudo rm -rf /etc/mysql /var/lib/mysql /var/lib/mysql-*
-
-sudo apt autoremove
